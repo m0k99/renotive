9c1da6340e24944beb61727dffb877bec5064afc
Mohammadreza Khalilikhorram <mr.khkh1999@gmail.com>
2024-10-24 23:56:16 +0330

    chore: add Tailwind CSS and remove unused components/pages

--- src/views/ProductSList.vue
+++ src/views/ProductSList.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <h1 class="text-5xl font-bold underline text-red-600">
+  <h1 class="text-3xl font-semibold underline bg-red-600">
     Hello world!
   </h1>
 </template>
