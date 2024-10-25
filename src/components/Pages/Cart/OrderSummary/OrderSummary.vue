3b100d0c2e1c954b3c6ab8a81e6290b2bc4700eb
Mohammadreza Khalilikhorram <mr.khkh1999@gmail.com>
2024-10-25 20:41:19 +0330

    feat: create static order summary component
    - Added a static order summary component to display order details.
    - Included layout for items, quantities, and total price.
    - Improved UI for better user experience in the checkout process.
    - Add lang fr and en

---
+++ src/components/Pages/Cart/OrderSummary/OrderSummary.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-gray-50 rounded-md p-8 text-sm space-y-6">
+    <h2 class="text-lg font-medium">
+      {{ $t('Order summary') }}
+    </h2>
+    <div class="grid grid-cols-1 divide-y">
+      <div class="flex items-center justify-between gap-3 pb-4">
+        <span class="text-gray-700">{{ $t('Total Before Discount') }}</span>
+        <span class="text-gray-900">
+          $99
+        </span>
+      </div>
+      <div class="flex items-center justify-between gap-3 py-4">
+        <span class="text-gray-700">{{ $t('Savings') }}</span>
+        <span class="text-gray-900">
+          $99
+        </span>
+      </div>
+      <div class="flex items-center justify-between gap-3 pt-4 text-base font-medium">
+        <span class="text-gray-900">{{ $t('Total After Savings') }}</span>
+        <span class="text-gray-900">
+          $99
+        </span>
+      </div>
+    </div>
+    <button class="w-full text-white py-3 bg-indigo-600 rounded-md font-medium">
+      {{ $t('Checkout') }}
+    </button>
+  </div>
+</template>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-
+import OrderSummary from '@/components/Pages/Cart/OrderSummary/OrderSummary.vue'
 </script>
 
 <template>
+  <div class="bg-white">
+    <div class="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8 space-y-12">
+      <h2 class="text-4xl font-bold tracking-tight text-gray-900">{{ $t('Shopping Cart') }}</h2>
 
+      <div class="grid grid-cols-12 gap-16">
+        <div class="col-span-7 border-t">
+          <div class="py-10 border-b">
+            Product
+          </div>
+        </div>
+        <div class="col-span-5">
+          <OrderSummary/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
