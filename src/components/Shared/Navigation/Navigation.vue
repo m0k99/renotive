ae9c157feec66be5e05b58d37bf95e5039be9464
Mohammadreza Khalilikhorram <mr.khkh1999@gmail.com>
2024-10-25 22:33:29 +0330

    fix: make navigation logo center in mobile size

--- src/components/Shared/Navigation/Navigation.vue
+++ src/components/Shared/Navigation/Navigation.vue
@@ -3,9 +3,9 @@
     <template v-slot="{ open }">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 justify-between">
-          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div class="flex items-center sm:hidden pr-16">
             <DisclosureButton
-              class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
               <span class="sr-only">Open main menu</span>
               <component :is="open ? XMarkIcon : Bars3Icon" class="block h-6 w-6" />
             </DisclosureButton>
