97f6b82276d912dd0dcb65de6383f26f5a02e385
Mohammadreza Khalilikhorram <mr.khkh1999@gmail.com>
2024-10-26 00:17:50 +0330

    feat: add empty state component with i18n support
    - Added "No Items in Cart" empty state component with title, subtitle, and ShoppingCartIcon.
    - Styled component using Tailwind CSS for a responsive and clean design.
    - Integrated i18n for title and subtitle text to support multiple languages.

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import OrderSummary from '@/components/Pages/Cart/OrderSummary/OrderSummary.vue'
 import OrderProducts from '@/components/Pages/Cart/OrderProducts/OrderProducts.vue'
+import { useCartStore } from '@/stores/useCartStore'
+import { toRef } from 'vue'
+import EmptyState from '@/components/Pages/Cart/EmptyState/EmptyState.vue'
+
+const cartStore = useCartStore()
+
+const cartProducts = toRef(cartStore, 'cartProducts')
+
 </script>
 
 <template>
@@ -8,13 +16,16 @@ import OrderProducts from '@/components/Pages/Cart/OrderProducts/OrderProducts.v
     <div class="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8 space-y-12">
       <h2 class="text-4xl font-bold tracking-tight text-gray-900">{{ $t('Shopping Cart') }}</h2>
 
-      <div class="grid grid-cols-6 lg:grid-cols-12 gap-16">
+      <div v-if="cartProducts.length" class="grid grid-cols-6 lg:grid-cols-12 gap-16">
         <div class="col-span-6 lg:col-span-7">
           <OrderProducts />
         </div>
         <div class="col-span-6 lg:col-span-5">
           <OrderSummary />
         </div>
+      </div>
+      <div v-else>
+        <EmptyState />
       </div>
     </div>
   </div>
